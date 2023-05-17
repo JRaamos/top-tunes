f26a788040dcf07179706a6f69ca4e48d8225e33
Jonathan Ramos <jhonyramos46@gmail.com>
2023-05-17 19:05:16 -0300

    requisito 08

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,28 +1,61 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class MusicCard extends Component {
+  state = {
+    carregando: false,
+    musicas: [],
+  };
+
+  favoritoSong = async (musica) => {
+    this.setState({ carregando: true });
+    await addSong(musica);
+    this.setState((prev) => ({
+      ...prev,
+      musicas: musica,
+      carregando: false,
+    }));
+  };
+
   render() {
     const { albunsFilter } = this.props;
+    const { carregando } = this.state;
+
     return (
       <div>
+        {carregando && <Carregando /> }
+
         {
-          albunsFilter.map(({ artistName, artworkUrl60, trackName, previewUrl,
-          }, index) => (
+          albunsFilter.map((element, index) => (
             <div
               key={ index }
             >
               <img
-                src={ artworkUrl60 }
-                alt={ artistName }
+                src={ element.artworkUrl60 }
+                alt={ element.artistName }
               />
-              <p>{trackName}</p>
-              <audio data-testid="audio-component" src={ previewUrl } controls>
+
+              <label>
+                Favorito
+
+                <input
+                  onChange={ ({ target }) => target.checked
+                  && this.favoritoSong(element) }
+                  type="checkbox"
+                  data-testid={ `checkbox-music-${element.trackId}` }
+                />
+
+              </label>
+              <p>{element.trackName}</p>
+              <audio data-testid="audio-component" src={ element.previewUrl } controls>
                 <track kind="captions" />
                 O seu navegador não suporta o elemento
                 <code>audio</code>
               </audio>
             </div>
+
           ))
         }
       </div>
@@ -35,6 +68,7 @@ MusicCard.propTypes = {
     artworkUrl60: PropTypes.string.isRequired,
     trackName: PropTypes.string.isRequired,
     previewUrl: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   })).isRequired,
 };
 export default MusicCard;
