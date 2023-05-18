823c00b3ba620dce5ea58f7865f026f7b815236d
Jonathan Ramos <jhonyramos46@gmail.com>
2023-05-18 13:40:08 -0300

    requisito 10

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,21 +1,42 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Carregando from './Carregando';
 
 class MusicCard extends Component {
   state = {
     carregando: false,
-    musicas: [],
+    check: '',
+    elements: [],
+    favorites: '',
   };
+
+  async componentDidMount() {
+    const { favorites } = this.state;
+    const music = await getFavoriteSongs();
+    const checked = music.some((musicas) => musicas.trackId === favorites.trackId);
+    this.setState((prev) => ({
+      ...prev,
+      elements: music,
+      check: checked,
+    }));
+  }
 
   favoritoSong = async (musica) => {
     this.setState({ carregando: true });
     await addSong(musica);
-    this.setState((prev) => ({
-      ...prev,
-      musicas: musica,
+    this.setState(() => ({
       carregando: false,
+      favorites: musica,
+    }));
+  };
+
+  removeFavoritoSong = async (musica) => {
+    this.setState({ carregando: true });
+    await removeSong(musica);
+    this.setState(() => ({
+      carregando: false,
+      favorites: musica,
     }));
   };
 
@@ -26,12 +47,12 @@ class MusicCard extends Component {
     return (
       <div>
         {carregando && <Carregando /> }
-
         {
           albunsFilter.map((element, index) => (
             <div
               key={ index }
             >
+
               <img
                 src={ element.artworkUrl60 }
                 alt={ element.artistName }
@@ -41,13 +62,14 @@ class MusicCard extends Component {
                 Favorito
 
                 <input
-                  onChange={ ({ target }) => target.checked
-                  && this.favoritoSong(element) }
+                  onChange={ ({ target }) => (target.checked
+                    ? this.favoritoSong(element) : this.removeFavoritoSong(element)) }
                   type="checkbox"
                   data-testid={ `checkbox-music-${element.trackId}` }
-                />
 
+                />
               </label>
+
               <p>{element.trackName}</p>
               <audio data-testid="audio-component" src={ element.previewUrl } controls>
                 <track kind="captions" />
